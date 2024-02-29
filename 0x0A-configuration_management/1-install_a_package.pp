ef354a60308d0999240b11620199331915afa2eb
petepresh007 <peterprecious370@gmail.com>
2024-02-29 05:09:03 -0800

    fixed task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# using puppet to instal flask (2.1.0)
-
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
