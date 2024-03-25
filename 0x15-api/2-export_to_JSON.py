dd50cc046aceadabcaa6f55ea4d6759654b4004d
petepresh007 <peterprecious370@gmail.com>
2024-03-25 12:39:53 -0700

    json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Export api to a csv format"""
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+    userID = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/' + userID
+    res = requests.get(url)
+    username = res.json().get('username')
+    task = url + '/todos'
+    res = requests.get(task)
+    tasks = res.json()
+
+    data = {userID: []}
+    for task in tasks:
+        comp_task = task.get("complete")
+        task_title = task.get("title")
+        data[userID].append({
+            "task": task_title,
+            "completed": comp_task,
+            "username": username
+            })
+    with open("{}.json".format(userID), "w") as f:
+        json.dump(data, f)
