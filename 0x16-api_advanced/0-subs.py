8e41f99a8000001e0e66b6c590172bafa24221b9
peter precious <peterprecious370@gmail.com>
2024-04-09 05:47:09 -0700

    task 1

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""a module to import"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """module to send a requests"""
+    headers = {'User-Agent': 'MyBot/1.0'}
+    red_api = requests.get(
+            f"https://www.reddit.com/r/{subreddit}/about.json",
+            headers=headers
+            )
+    if red_api.status_code == 200:
+        data = red_api.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+    elif red_api.status_code == 404:
+        return 0
